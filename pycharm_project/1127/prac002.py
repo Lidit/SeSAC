3587cc8361f0e017b1f732eac4e2ea7b3d42977b
Lidit <dropzonekr@gmail.com>
2023-11-27 18:41:24 +0900

    1127 수업내용 커밋 (주석 추가 및 레거시 삭제)

--- pycharm_project/1127/prac002.py
+++ pycharm_project/1127/prac002.py
@@ -7,39 +7,6 @@ from torchsummary import summary
 class VGG11(nn.Module):
     def __init__(self):
         super(VGG11, self).__init__()
-        #
-        # self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1, stride=1)
-        # self.act1 = nn.ReLU()
-        # self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        #
-        # self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1, stride=1)
-        # self.act2 = nn.ReLU()
-        # self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        #
-        # self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1, stride=1)
-        # self.act3 = nn.ReLU()
-        # self.conv4 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1, stride=1)
-        # self.act4 = nn.ReLU()
-        # self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        #
-        # self.conv5 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, stride=1)
-        # self.act5 = nn.ReLU()
-        # self.conv6 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=1)
-        # self.act6 = nn.ReLU()
-        # self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
-        #
-        # self.conv7 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=1)
-        # self.act7 = nn.ReLU()
-        # self.conv8 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=1)
-        # self.act8 = nn.ReLU()
-        # self.pool5 = nn.MaxPool2d(kernel_size=2, stride=2)
-        #
-        # self.fc1 = nn.Linear(in_features=512*7*7, out_features=4096)
-        # self.act9 = nn.ReLU()
-        # self.fc2 = nn.Linear(in_features=4096, out_features=4096)
-        # self.act10 = nn.ReLU()
-        # self.fc3 = nn.Linear(in_features=4096, out_features=1000)
-
         self.layer1 = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1, stride=1),
             nn.ReLU(),
@@ -77,6 +44,7 @@ class VGG11(nn.Module):
         )
 
         self.fc_layer = nn.Sequential(
+            # MaxPool 된 데이터의 shape 고려하여 in_features 설정
             nn.Linear(in_features=512 * 7 * 7, out_features=4096),
             nn.ReLU(),
             nn.Linear(in_features=4096, out_features=4096),
@@ -91,36 +59,7 @@ class VGG11(nn.Module):
         x = self.layer3(x)
         x = self.layer4(x)
         x = self.layer5(x)
-        # print(self.layer5.eval())
-        # print(x.shape)
-        # print(x.view())
         x = self.fc_layer(x.view(-1, 512 * 7 * 7))
-
-        #
-        # x = self.act1(self.conv1(x))
-        # x = self.pool1(x)
-        #
-        # x = self.act2(self.conv2(x))
-        # x = self.pool2(x)
-        #
-        # x = self.act3(self.conv3(x))
-        # x = self.act4(self.conv4(x))
-        # x = self.pool3(x)
-        #
-        # x = self.act5(self.conv5(x))
-        # x = self.act6(self.conv6(x))
-        # x = self.pool4(x)
-        #
-        # x = self.act7(self.conv7(x))
-        # x = self.act8(self.conv8(x))
-        # x = self.pool5(x)
-        # print(x.shape)
-        #
-        # x = self.act9(self.fc1(x.view(x.size(0),-1)))
-        #
-        # x = self.act10(self.fc2(x))
-        #
-        # x = self.fc3(x)
 
         return x
 
@@ -130,8 +69,7 @@ input_tensor = torch.rand(size=(C, H, W))
 
 model = VGG11()
 
-# model.forward(input_tensor)
 print(summary(model=model, input_size=(C, H, W), batch_size=1))
 
-out =model.forward(input_tensor)
+out = model.forward(input_tensor)
 print(out.shape)
